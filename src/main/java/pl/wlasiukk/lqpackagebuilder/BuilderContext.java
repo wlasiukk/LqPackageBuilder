0e8e501ee59216e6c908e1ee013d97afde2f2399
kwlasiuk <krzysztof.wlasiuk@gemini.pl>
2024-01-28 13:30:11 +0100

    version 0.1.2

--- src/main/java/pl/wlasiukk/lqpackagebuilder/BuilderContext.java
+++ src/main/java/pl/wlasiukk/lqpackagebuilder/BuilderContext.java
@@ -262,7 +262,7 @@ public class BuilderContext {
     }
 
     public static String getVersion() {
-        return "0.1.1";
+        return "0.1.2";
     }
 
     public String getFullOutputDirectoryPath() {
